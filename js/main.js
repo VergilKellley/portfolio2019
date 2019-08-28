33801c75dccb3b61f445cdc3a7b4b1858be20c5c
Vergil Kelley <verg_1@yahoo.com>
2019-08-27 22:33:47 -0500

    changed speed of slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,64 @@
+// get weather for current location
+getWeather.addEventListener("click", () => {
+  let long;
+  let lat;
+  let temperatureDescription = document.querySelector(
+    ".temperature-description"
+  );
+  let temperatureDegree = document.querySelector(".temperature-degree");
+  let locationTimeZone = document.querySelector(".location-timezone");
+  let temperatureSection = document.querySelector(".temperature");
+  const temperatureSpan = document.querySelector(".temperature span");
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(Position => {
+      long = Position.coords.longitude;
+      lat = Position.coords.latitude;
+
+      const proxy = "https://cors-anywhere.herokuapp.com/";
+      const api = `${proxy}https://api.darksky.net/forecast/f096a2ce913f2b10a500d5564399fcb7/${lat},${long}`;
+
+      fetch(api)
+        .then(Response => {
+          return Response.json();
+        })
+        .then(data => {
+          console.log(data);
+          const { temperature, summary, icon } = data.currently;
+          // set DOM elements from the api
+          temperatureDegree.textContent = Math.floor(temperature);
+          temperatureDescription.textContent = summary;
+          locationTimeZone.textContent = data.timezone;
+          // celsius formula
+          let celsius = (temperature - 32) * (5 / 9);
+          // set icon
+          setIcons(icon, document.querySelector(".icon"));
+
+          // change temperature to celcius/farenheit
+          temperatureSection.addEventListener("click", () => {
+            if (temperatureSpan.textContent === "F") {
+              temperatureSpan.textContent = "C";
+              temperatureDegree.textContent = Math.floor(celsius);
+            } else {
+              temperatureSpan.textContent = "F";
+              temperatureDegree.textContent = Math.floor(temperature);
+            }
+          });
+        });
+    });
+  } else {
+    h1.textContent = "Reload page and click allow to see local weather";
+  }
+  // get weather icons
+  function setIcons(icon, iconID) {
+    const skycons = new Skycons({ color: "white" });
+    const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+    skycons.play();
+    return skycons.set(iconID, Skycons[currentIcon]);
+  }
+});
+
+// slider function
 const slides = document.querySelectorAll(".slide");
 const next = document.querySelector("#next");
 const prev = document.querySelector("#prev");
